18c878cbbbd86f6ca05ce0f0f8e54647ff8146d6
Maxim Harlan <harlan_max@mail.ru>
2025-09-21 16:59:13 +0300

    Создан менеджер по работе с заданиями, а также созданы проверочный тесты в классе Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,45 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager manager = new TaskManager();
+
+        // 2 Задачи
+        Task task1 = manager.createTask(new Task("Посмотреть погоду", "Для выезда за город"));
+        Task task2 = manager.createTask(new Task("Разгрузить подвал", "Чтобы освободить место"));
+
+        // 2 подзадачи
+        Epic epic1 = manager.createEpic(new Epic("Переезд", "Организовать переезд в новую квартиру"));
+        Subtask subtask1 = manager.createSubtask(new Subtask("Собрать коробки", "Собрать вещи в коробки", epic1.getId()));
+        Subtask subtask2 = manager.createSubtask(new Subtask("Разобрать мебель", "Разобрать старую мебель", epic1.getId()));
+
+        // 1 подзадача
+        Epic epic2 = manager.createEpic(new Epic("Отпуск", "Спланировать летний отпуск"));
+        Subtask subtask3 = manager.createSubtask(new Subtask("Купить билеты", "Билеты на самолет", epic2.getId()));
+
+        System.out.println("--- Начальное состояние ---");
+        System.out.println("Все задачи: " + manager.getAllTasks());
+        System.out.println("Все эпики: " + manager.getAllEpics());
+        System.out.println("Все подзадачи: " + manager.getAllSubtasks());
+
+        // Обновление статуса
+        System.out.println("\n--- Обновление статусов ---");
+        System.out.println("Статус эпика 'Переезд' изначально: " + manager.getEpicById(epic1.getId()).getStatus());
+        subtask1.setStatus(TaskStatus.DONE);
+        manager.updateSubtask(subtask1);
+        System.out.println("Статус эпика 'Переезд' после завершения одной подзадачи: " + manager.getEpicById(epic1.getId()).getStatus());
+
+        subtask2.setStatus(TaskStatus.DONE);
+        manager.updateSubtask(subtask2);
+        System.out.println("Статус эпика 'Переезд' после завершения всех подзадач: " + manager.getEpicById(epic1.getId()).getStatus());
+
+        // Удаление
+        System.out.println("\n--- Удаление задач ---");
+        manager.deleteTaskById(task2.getId());
+        manager.deleteEpicById(epic2.getId());
+        System.out.println("Все задачи после удаления: " + manager.getAllTasks());
+        System.out.println("Все эпики после удаления: " + manager.getAllEpics());
+        System.out.println("Все подзадачи после удаления эпика 'Отпуск': " + manager.getAllSubtasks());
     }
 }
