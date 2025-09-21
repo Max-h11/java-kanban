48a3736d84f8a09d74cdbffd2b13963d360c78e2
Maxim Harlan <harlan_max@mail.ru>
2025-09-21 16:33:40 +0300

    Создан класс с логикой подзадачи

---
+++ src/Subtask.java
@@ -0,0 +1,32 @@
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(String name, String description, int epicId) {
+        super(name, description);
+        this.epicId = epicId;
+    }
+
+    public Subtask(int id, String name, String description, TaskStatus status, int epicId) {
+        super(id, name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "epicId=" + epicId +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
